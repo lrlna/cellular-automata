038950d3e8cf55c47dc15a465661b9bc2497f73a
lrlna <shestak.irina@gmail.com>
2017-01-01 03:02:05 +0100

    Updates

--- bundle.js
+++ bundle.js
@@ -2507,7 +2507,7 @@ function Automata () {
 
   // need some automata accessible variables
   ctx.automata = []
-  ctx.rule
+  ctx.rule = ''
 
   // create automata lattice based on size (number of cells) provided
   // run over a set number of a given life
@@ -2516,6 +2516,9 @@ function Automata () {
     assert.ok(rule, 'olivaw: you need a rule for olivaw to run')
 
     var lattice = []
+
+    // clear automata array 
+    ctx.automata = []
     lattice = ctx.setState(lattice, number)
     lattice = ctx.setNeighbours(lattice)
     ctx.rule = ctx.getRulesBinary(rule)
